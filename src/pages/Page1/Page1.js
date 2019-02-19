c61dc6173665e74297a07b2ab864295ff827f263
Alvin1314 <18662333225@163.com>
2019-02-19 16:36:52 +0800

    webpack.dev.config添加css和img配置

--- src/pages/Page1/Page1.js
+++ src/pages/Page1/Page1.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
+import "./Page1.css";
+import imageUrl from "./images/timg.jpg"
 
 export default class Page1 extends Component {
   render() {
-    return <div>this is Page1~</div>;
+    return (
+      <div className="page-box">
+        this is Page1~
+        <img src={imageUrl} />>
+      </div>
+    );
   }
 }
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -9,7 +9,7 @@ module.exports = {
     path: path.join(__dirname, "./dist"),
     filename: "bundle.js"
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   /*src文件夹下面的以.js结尾的文件，要使用babel解析*/
   /*cacheDirectory是用来缓存编译结果，下次编译加速*/
   module: {
@@ -18,6 +18,21 @@ module.exports = {
         test: /\.js$/,
         use: ["babel-loader?cacheDirectory=true"],
         include: path.join(__dirname, "src")
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192
+            }
+          }
+        ]
       }
     ]
   },
@@ -27,7 +42,7 @@ module.exports = {
       component: path.join(__dirname, "src/component"),
       router: path.join(__dirname, "src/router"),
       actions: path.join(__dirname, "src/redux/actions"),
-      reducers: path.join(__dirname, "src/redux/reducers"),
+      reducers: path.join(__dirname, "src/redux/reducers")
     }
   },
   devServer: {
